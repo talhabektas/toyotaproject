984ff16e659a31715ca9d923d958dd2be1e6696e
talhabektas <mehmettalha.bektas@gmail.com>
2025-03-10 20:43:47 +0300

    init

---
+++ src/main/java/com/example/cltoyota/CltoyotaApplication.java
@@ -0,0 +1,13 @@
+package com.example.cltoyota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CltoyotaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CltoyotaApplication.class, args);
+    }
+
+}
